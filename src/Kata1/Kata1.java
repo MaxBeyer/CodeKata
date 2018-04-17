9f26d821d4fd1cc7732cf953b171220d4af01a9b
Max Beyer <inflatableanchor@gmail.com>
2018-04-17 15:24:41 -0500

    test commit from eclipse

--- src/Kata1/Kata1.java
+++ src/Kata1/Kata1.java
@@ -1,6 +1,7 @@
 package Kata1;
 public class Kata1 {
 	
+	//main method used for random numeral verification
 	public static void main(String[] args) {
 		NumberConversion numberConversion = new NumberConversion();
 		Integer number = 1234;
